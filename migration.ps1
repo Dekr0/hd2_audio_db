7ad5b1aec73070d9cf279056965e7d92771fbfb0
Dekr0 <dekr0.dk@protonmail.com>
2024-10-09 06:31:32 -0600

    source script to export necessary environemntal variable for goose migration

---
+++ migration.ps1
@@ -0,0 +1,3 @@
+$Env:GOOSE_DBSTRING="./hd_audio_db.db"
+$Env:GOOSE_MIGRATION_DIR="sql/schema"
+$Env:GOOSE_DRIVER="sqlite3"
---
+++ migration.sh
@@ -0,0 +1,3 @@
+export GOOSE_DBSTRING="./hd_audio_db.db"
+export GOOSE_MIGRATION_DIR="sql/schema"
+export GOOSE_DRIVER="sqlite3"
