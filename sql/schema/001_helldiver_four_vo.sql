241b5d5a5835c61c58a73f3c90f4e314756be4af
Dekr0 <dekr0.dk@protonmail.com>
2024-10-11 10:50:22 -0600

    file_id is not longer unique; no  behind the nouns

--- sql/schema/001_helldiver_four_vo.sql
+++ sql/schema/001_helldiver_four_vo.sql
@@ -1,13 +1,13 @@
 -- +goose Up
-CREATE TABLE helldiver_four_voice_lines (
+CREATE TABLE helldiver_four_voice_line (
     id TEXT PRIMARY KEY,
-    file_id TEXT NOT NULL UNIQUE,
+    file_id TEXT NOT NULL,
     transcription TEXT NOT NULL
 );
 
 CREATE VIRTUAL TABLE helldiver_four_vo_fts USING fts5(id, file_id, transcription);
 
 -- +goose Down
-DROP TABLE helldiver_four_voice_lines;
+DROP TABLE helldiver_four_voice_line;
 
 DROP TABLE helldiver_four_vo_fts;
