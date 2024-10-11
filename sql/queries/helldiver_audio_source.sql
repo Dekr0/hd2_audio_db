97db2abea1a75c029d1dd1caf8323ca9d76d7523
Dekr0 <dekr0.dk@protonmail.com>
2024-10-11 10:52:42 -0600

    change data type for audio source table; more column attribute; reflect insert query

--- sql/queries/helldiver_audio_source.sql
+++ sql/queries/helldiver_audio_source.sql
@@ -1,5 +1,5 @@
 INSERT INTO helldiver_audio_source (
-    id, audio_source_id, relative_archive_ids
+    id, audio_source_id, relative_archive_ids, relative_archive_tags
 ) VALUES (
-    ?, ?, ?
+    ?, ?, ?, ?
 );
