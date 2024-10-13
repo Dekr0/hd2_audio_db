0a756c14ab5c31a5c352528f731432c6f4e4559a
Dekr0 <dekr0.dk@protonmail.com>
2024-10-13 15:58:33 -0600

    query adjustment to reflect schema changes

--- sql/queries/helldiver_four_vo.sql
+++ sql/queries/helldiver_four_vo.sql
@@ -1,13 +1,13 @@
 -- name: CreateHelldiverFourTranscription :exec
 INSERT INTO helldiver_four_voice_line (
-    id, file_id, transcription
+    helldiver_four_voice_line_id, file_id, transcription
 ) VALUES (
     ?, ?, ?
 );
 
 -- name: CreateHelldiverFourVOFTSEntry :exec
 INSERT INTO helldiver_four_vo_fts (
-    id, file_id, transcription
+    helldiver_four_voice_line_id, file_id, transcription
 ) VALUES (
     ?, ?, ?
 );
