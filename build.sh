32e39f7d064f27fc6cf04e2df2afa0b574af9337
Dekr0 <dekr0.dk@protonmail.com>
2024-10-17 05:47:07 -0600

    build script for windows

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CXX=x86_64-w64-mingw32-g++ CC=x86_64-w64-mingw32-gcc go build .
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CXX=x86_64-w64-mingw32-g++ CC=x86_64-w64-mingw32-gcc go build . -o hda.exe
+cp hda.exe ./build
+cp ./.env ./build
