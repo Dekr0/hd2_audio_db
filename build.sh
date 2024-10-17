962087bad153fc58c9d7dea7053d9933e6eb9d72
Dekr0 <dekr0.dk@protonmail.com>
2024-10-17 05:49:14 -0600

    build script misc

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CXX=x86_64-w64-mingw32-g++ CC=x86_64-w64-mingw32-gcc go build . -o hda.exe
-cp hda.exe ./build
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CXX=x86_64-w64-mingw32-g++ CC=x86_64-w64-mingw32-gcc go build -o hda.exe
+mv hda.exe ./build
 cp ./.env ./build
