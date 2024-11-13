98948a4deab5c6cc7305a4e2332b10f781dfd732
Dekr0 <dekr0.dk@protonmail.com>
2024-11-13 03:50:44 -0700

    rename columns and tables

--- sql/schema/001_helldiver_audio_entity.sql
+++ sql/schema/001_helldiver_audio_entity.sql
@@ -2,14 +2,16 @@
 CREATE TABLE helldiver_game_archive (
     id TEXT PRIMARY KEY,
     game_archive_id TEXT NOT NULL UNIQUE,
+    tags TEXT NOT NULL,
     categories TEXT NOT NULL
 );
 
-CREATE TABLE helldiver_audio_bank (
+CREATE TABLE helldiver_soundbank (
     id TEXT PRIMARY KEY,
-    audio_bank_id TEXT NOT NULL UNIQUE,
-    audio_bank_name TEXT NOT NULL,
-    category TEXT NOT NULL,
+    soundbank_id TEXT NOT NULL UNIQUE,
+    soundbank_name TEXT NOT NULL UNIQUE,
+    soundbank_readable_name TEXT NOT NULL,
+    categories TEXT NOT NULL,
     linked_game_archive_ids TEXT NOT NULL
 );
 
@@ -18,10 +20,10 @@ CREATE TABLE helldiver_audio_source (
     audio_source_id TEXT NOT NULL UNIQUE,
     label TEXT NOT NULL,
     tags TEXT NOT NULL,
-    linked_audio_bank_id TEXT NOT NULL
+    linked_soundbank_ids TEXT NOT NULL
 );
 
 -- +goose Down
 DROP TABLE helldiver_audio_source;
-DROP TABLE helldiver_audio_bank;
+DROP TABLE helldiver_soundbank;
 DROP TABLE helldiver_game_archive;
