b5b7b736fb9cbf065100bdd11126d4c7c8eb1619
Dekr0 <dekr0.dk@protonmail.com>
2024-11-23 03:56:38 -0700

    update schema to incorporate the new information from Wwiser

--- sql/schema/001_helldiver_audio_entity.sql
+++ sql/schema/001_helldiver_audio_entity.sql
@@ -15,12 +15,32 @@ CREATE TABLE helldiver_soundbank (
     linked_game_archive_ids TEXT NOT NULL
 );
 
-CREATE TABLE helldiver_audio_source (
+CREATE TABLE helldiver_hirearchy_object_type (
     id TEXT PRIMARY KEY,
+    type TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE helldiver_hirearchy_object (
+    id TEXT PRIMARY KEY,
+    wwise_id TEXT NOT NULL UNIQUE,
+    type TEXT NOT NULL,
+    parent_wwise_id TEXT NOT NULL,
+    FOREIGN KEY (object_type) REFERENCES helldiver_hirearchy_object_type(id)
+);
+
+CREATE TABLE helldiver_random_seq_container (
+    id TEXT PRIMARY KEY,
+    sounds TEXT NOT NULL,
+    FOREIGN KEY (id) REFERENCES helldiver_hirearchy_object(id)
+);
+
+CREATE TABLE helldiver_audio_source (
+    id PRIMARY KEY,
     audio_source_id TEXT NOT NULL UNIQUE,
     label TEXT NOT NULL,
     tags TEXT NOT NULL,
-    linked_soundbank_ids TEXT NOT NULL
+    linked_soundbank_ids TEXT NOT NULL,
+    FOREIGN KEY (id) REFERENCES helldiver_hirearchy_object(id)
 );
 
 -- +goose Down
