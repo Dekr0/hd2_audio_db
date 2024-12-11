d02bd25f62321f08c976f9bd295b070efb7ed4c9
Dekr0 <dekr0.dk@protonmail.com>
2024-12-11 10:50:35 -0700

    misc

--- db_test.go
+++ db_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestUpdateHelldiverSoundbanks(t *testing.T) {
 	DATA = "D:\\Program Files\\Steam\\steamapps\\common\\Helldivers 2\\data"
-	err := extractHelldiverSoundbanks(context.Background())
+	err := extractAllSoundbanks(context.Background())
 	if err != nil {
 		t.Fatal(err)
 	}
