81c02b2ae8b3bcf6dfbc89a8696e24a82effb1ea
Dekr0 <dekr0.dk@protonmail.com>
2025-01-30 15:54:17 -0700

    Work in Progress SQL query for updating label and description

--- sql/queries/audio_data_update.sql
+++ sql/queries/audio_data_update.sql
@@ -1,2 +1,29 @@
--- name: UpdateSoundLabelBySourceId :exec
-UPDATE sound SET label = ? WHERE wwise_short_id = ?;
+-- name: UpdateLabelByWwiseObjectId :exec
+UPDATE hierarchy_object SET label = ? WHERE wwise_object_id = ?;
+
+-- name: UpdateDescriptionByWwiseObjectId :exec
+UPDATE hierarchy_object SET description = ? WHERE wwise_object_id = ?;
+
+-- name: UpdateInfoByWwiseObjectId :exec
+UPDATE hierarchy_object SET label = ?, description = ? WHERE wwise_object_id = ?;
+
+-- name: UpdateLabelByWwiseShortId :exec
+UPDATE hierarchy_object SET label = ?
+WHERE hierarchy_object.db_id IN (
+    SELECT sound.db_id FROM sound 
+    WHERE sound.wwise_short_id = ?
+);
+
+-- name: UpdateDescByWwiseShortId :exec
+UPDATE hierarchy_object SET description = ?
+WHERE hierarchy_object.db_id IN (
+    SELECT sound.db_id FROM sound
+    WHERE sound.wwise_short_id = ?
+);
+
+-- name: UpdateInfoByWwiseShortId :exec
+UPDATE hierarchy_object SET label = ?, description = ?
+WHERE hierarchy_object.db_id IN (
+    SELECT sound.db_id FROM sound
+    WHERE sound.wwise_short_id = ?
+);
