23f78ba7c839c45f4b071e82589be96711849766
Dekr0 <dekr0.dk@protonmail.com>
2025-05-18 00:13:36 -0600

    update select query

--- sql/queries/audio_data_query.sql
+++ sql/queries/audio_data_query.sql
@@ -1,6 +1,2 @@
--- name: GetAllGameArchives :many
-SELECT * FROM game_archive;
-
--- name: GetAllHierarchyObjectTypes :many
-SELECT * FROM hierarchy_object_type;
-
+-- name: GetAllArchive :many
+SELECT * FROM archive;
