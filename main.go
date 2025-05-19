d07275022f2e80911ceee2b79840cef43ec9c059
Dekr0 <dekr0.dk@protonmail.com>
2025-05-19 05:46:05 -0600

    rework main

--- main.go
+++ main.go
@@ -2,118 +2,147 @@ package main
 
 import (
 	"context"
+	"dekr0/hd2_audio_db/db"
 	"flag"
+	"fmt"
+	"log/slog"
 	"os"
-
-    "dekr0/hd2_audio_db/db"
-    "dekr0/hd2_audio_db/log"
-    "dekr0/hd2_audio_db/toc"
-    "dekr0/hd2_audio_db/wwise"
+	"time"
 )
 
-func run() error {
-	if len(os.Args) > 3 {
-		log.DefaultLogger.Error("Only one option can run at the same time")
-	}
-
-	dataPathFlag := flag.String("data", "", 
-	"Absolute path to Helldivers 2 data directory")
-
-	archiveTableFlag := flag.Bool("table-archive", false, 
-	"Completely db.Rewrite basic information of all game archives (based on spreadsheets) into the DB" + 
-	" (Destructive). Used by DB maintainers")
-
-    allArchiveTableFlag := flag.Bool("table-archive-all", false,
-    "Completely db.Rewrite basic information of all game archives (not based on " +
-    "spreadsheets, based on content in the Helldivers 2 data directory) into the" +
-    " DB (Destructive). Used by DB maintainers")
-
-    bankTableFlag := flag.Bool("table-bank", false,
-    "Completely db.Rewrite basic information about Wwise Soundbank into the DB" +
-    "(Destructive). Used by DB maintainers.")
-
-	hierarchyObjTableFlag := flag.Bool("table-hierarchy-object", false,
-	"Completely db.Rewrite information about Wwise Soundbank and its hierarchy " +
-	" objects used in game into the DB (Destructive). Used by DB maintainers.")
-
-	streamTableFlag := flag.Bool("table-stream", false, 
-	"Completely db.Rewrite information about all Wwise streams used in game into" +
-	" the DB (Destructive). Used by DB maintainers.")
-
-	extractBankFlag := flag.String("extract-bank", "", 
-	"Extract basic information of Wwise Soundbanks in (a) game archive(s), Wwise " +
-	"Soundbank binary content, and its Wwiser XML output")
-
-    labelDirFlag := flag.String("import-label-folder", "",
-    "Import label and description for hierarchy objects. Accept a abs. / relative" +
-    " path of a directory that contains JSON files.",
-    )
-
-	xmlFlag := flag.String("xmls", "", "")
-
-    if *dataPathFlag != "" {
-        toc.DATA = *dataPathFlag
-    } else {
-        toc.DATA = os.Getenv("HELLDIVER2_DATA")
-    }
-
-    db.CSV_DIR = os.Getenv("CSV_DIR")
-    if db.CSV_DIR == "" {
-        db.CSV_DIR = "./csv/archives/"
-    }
+func main() {
+	insertArchive := flag.Bool(
+		"insert_archive",
+		false, 
+		"Insert records of all archives in the `data` folder into `archive` " + 
+		"table.",
+	)
+	generate := flag.Bool(
+		"generate",
+		false,
+		"Populate records for `asset`, `soundbank`, `hierarchy`, and `sound`" +
+		" table.",
+	)
+	extractAllSoundbank := flag.Bool(
+		"extract_all_soundbank",
+		false,
+		"Extract sound banks from all archives in `data` folder",
+	)
+	extractSoundbank := flag.Bool(
+		"extract_soundbank",
+		false,
+		"Extract sound banks from selective archives." + 
+		"Multi-selective is enable. Use `Tab` to select / deselect",
+	)
+	extractSoundbankDB := flag.Bool(
+		"extract_soundbank_db",
+		false,
+		"Extract sound banks from selective archives. Each archive is tag by " + 
+		"a sound bank name. Multi-selective is enable. Use `Tab` to select / " +
+		"deselect",
+	)
+	data := flag.String("data", "", "")
+	dest := flag.String("dest", "", "")
 
 	flag.Parse()
 
-	ctx := context.Background()
-
-	if *archiveTableFlag {
-        if err := db.RewriteAllHierarchyObjectTypes(ctx); err != nil {
-            return err
-        }
-		return db.RewriteAllGameArchivesSpreadsheet("./csv/archives", ctx)
+	if *data != "" {
+		slog.Info("Using data path from argument.")
+		stat, err := os.Lstat(*data)
+		if err != nil {
+			slog.Error("Failed to query provided data path", "error", err)
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		if stat.IsDir() {
+			if err := os.Setenv("DATA", *data); err != nil {
+				slog.Error("Failed to use provided data path", "error", err)
+			}
+		} else {
+			slog.Error("Provided data path is a file")
+		}
 	}
 
-    if *allArchiveTableFlag {
-        if err := db.RewriteAllHierarchyObjectTypes(ctx); err != nil {
-            return err
-        }
-        return db.RewriteAllGameArchives(ctx)
-    }
-    
-    if *bankTableFlag {
-        return db.RewriteAllSoundBank(ctx)
-    }
-
-	if *hierarchyObjTableFlag {
-		return db.RewriteAllSoundAssets(ctx)
-	}
-
-	if *streamTableFlag {
-        return nil
-	}
-
-	if *extractBankFlag != "" {
-		toc.ExtractBank(*extractBankFlag)
-		return nil
-	}
-
-	if *xmlFlag != "" {
-		return wwise.ParseWwiserXML(*xmlFlag)
-	}
-
-    if *labelDirFlag != "" {
-        return db.UpdateSoundLabelsFromFolder(*labelDirFlag, ctx)
-    }
-
-	flag.Usage()
-
-	return nil
-}
-
-func main() {
-	if err := run(); err != nil {
-		log.DefaultLogger.Error("Error", "error_detail", err)
+	*data = os.Getenv("DATA")
+	stat, err := os.Lstat(*data)
+	if err != nil {
+		slog.Error("Failed to query provided data path", "error", err)
 		os.Exit(1)
 	}
-	os.Exit(0)
+	if !stat.IsDir() {
+		slog.Error("Provided data path is a file")
+		os.Exit(1)
+	}
+
+	if *insertArchive {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second * 12)
+		defer cancel()
+		if err := db.WriteArchives(ctx, *data); err != nil {
+			slog.Error("Failed to insert records into archive table", "error", err)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
+
+	if *generate {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second * 60)
+		defer cancel()
+		if err := db.Generate(ctx, *data); err != nil {
+			slog.Error(
+				"Populate records for `asset`, `soundbank`, `hierarchy`, and " + 
+				"`sound` table.",
+				"error", err,
+			)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
+
+	if *extractAllSoundbank  {
+		if *dest == "" {
+			slog.Error("Destination for output sound bank is not provided")
+			os.Exit(1)
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second * 16)
+		defer cancel()
+		if err := db.ExportAllSoundbank(ctx, *data, *dest); err != nil {
+			slog.Error("Failed to extract all sound banks", "error", err)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
+
+	if *extractSoundbank {
+		if *dest == "" {
+			slog.Error("Destination for output sound bank is not provided")
+			os.Exit(1)
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second * 16)
+		defer cancel()
+		if err := db.ExportSoundbanksTUI(ctx, *data, *dest); err != nil {
+			slog.Error("Failed to extract sound banks", "error", err)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
+
+	if *extractSoundbankDB {
+		if *dest == "" {
+			slog.Error("Destination for output sound bank is not provided")
+			os.Exit(1)
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second * 16)
+		defer cancel()
+		if err := db.ExportSoundbanksTUIDB(ctx, *data, *dest); err != nil {
+			slog.Error("Failed to extract sound banks", "error", err)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
+
+	flag.Usage()
 }
+
