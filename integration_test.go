6a0e1ea6fb2c000aba311282108fcca5ba6bd9bb
Dekr0 <dekr0.dk@protonmail.com>
2025-07-15 10:35:59 -0600

    integration test

---
+++ integration_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+	"dekr0/hd2_audio_db/db"
+	"os"
+	"testing"
+)
+
+func TestGenerate(t *testing.T) {
+	os.Setenv("DATA", "/mnt/d/Program Files/Steam/steamapps/common/Helldivers 2/data")
+	data := os.Getenv("DATA")
+	os.Setenv("GOOSE_DBSTRING", "build_15314")
+	ctx := context.Background()
+	if err := db.Generate(ctx, data); err != nil {
+		t.Fatal(err)
+	}
+}
