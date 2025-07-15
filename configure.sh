d664674dfa1c0ba89bdf775003c639155d389da4
Dekr0 <dekr0.dk@protonmail.com>
2025-07-15 10:33:38 -0600

    - update script so that it's more dynamic and configurable: GOOSE_DBSTRING is assigned by using VERSION value

--- configure.sh
+++ configure.sh
@@ -1,8 +1,8 @@
 export DATA="/mnt/d/Program Files/Steam/steamapps/common/Helldivers 2/data"
 export CGO_ENABLE=1
-export GOOSE_DBSTRING="build_15314"
-export GOOSE_MIGRATION_DIR="sql/schema"
 export GOOSE_DRIVER="sqlite3"
+
+VERSION=15314
 
 config() {
     go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
@@ -16,20 +16,48 @@ config() {
     sqlc generate
 }
 
-generate() {
+goose_up_complete() {
+    export GOOSE_DBSTRING="build_${VERSION}"
+    export GOOSE_MIGRATION_DIR="sql/schema/complete"
+
     if [ -e $GOOSE_DBSTRING ]; then
         mv $GOOSE_DBSTRING "${GOOSE_DBSTRING}_backup"
     fi
     goose up
+}
 
+goose_up_id() {
+    export GOOSE_DBSTRING="id_${VERSION}"
+    export GOOSE_MIGRATION_DIR="sql/schema/id"
+
+    if [ -e $GOOSE_DBSTRING ]; then
+        mv $GOOSE_DBSTRING "${GOOSE_DBSTRING}_backup"
+    fi
+    goose up
+}
+
+reset_log() {
     if [ -e log.txt ]; then
         rm log.txt
     fi
+}
+
+generate() {
+    goose_up_complete
+
+    reset_log
 
     go run . --insert_archive
     go run . --generate
 
     sqlite3 $GOOSE_DBSTRING < sql/view.sql 
+}
+
+export_id() {
+    goose_up_id
+    export BUILD="build_${VERSION}_backup"
+    reset_log
+    go run . --export_id
 }
 
 extract_all_soundbank() {
